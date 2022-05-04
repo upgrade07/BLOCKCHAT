16b7f6e1877f80972856344ee89a17302a5c116b
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2022-05-04 16:00:57 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import '../styles/globals.css'
 import { MoralisProvider } from "react-moralis";
 function MyApp({ Component, pageProps }) {
   return(
-    <MoralisProvider appId="JmDBgKhJhnj2xK84aGpCZJOcGU5SYJ6GAPNqJtAU"
-     serverUrl="https://clitrjrleikz.usemoralis.com:2053/server">
+    <MoralisProvider appId="RYiB2mjHNkv9RsejfQEe8XDUY0OT2kolRQvzie17"
+     serverUrl="https://v83p1bphxeqj.usemoralis.com:2053/server">
 <Component {...pageProps} />
     </MoralisProvider>
     
